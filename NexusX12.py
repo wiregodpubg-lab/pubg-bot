3ee6951a1c968ed5ebb21aadfa9d7da67de37333
wiregodpubg-lab <wiregodpubg@gmail.com>
2026-06-02 06:14:00 +0330

    Add files via upload

---
+++ NexusX12.py
@@ -0,0 +1,11 @@
+import telebot
+
+# Using the provided token
+TOKEN = '8981532484:AAGe0CuTyI5W6THRjcz6SkE5-9ao4Jf-C5Y'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Bot is online and running successfully!")
+
+bot.infinity_polling()
