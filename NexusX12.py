48a4dabc3542471ef0b2448f6c6d644f9a43488d
wiregodpubg-lab <wiregodpubg@gmail.com>
2026-06-02 18:26:36 +0330

    Delete NexusX12.py

--- NexusX12.py
+++
@@ -1,9 +0,0 @@
-import os
-from telegram.ext import Updater
-
-# خواندن توکن از تنظیمات رندر (Environment Variable)
-TOKEN = os.getenv('API_TOKEN')
-
-# شروع ربات
-updater = Updater(TOKEN)
-# ... بقیه کدهای ربات خودت را اینجا بنویس ...
